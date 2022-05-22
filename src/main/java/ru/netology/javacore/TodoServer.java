fb92b79200aecf924ab256ca992778fb49a5f786
Vlasov Mikhail <m.vlasov@dunice.net>
2022-05-22 21:46:39 +0300

    add some function

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -12,11 +12,19 @@ public class TodoServer {
     protected Todos todos;
 
     public TodoServer(int port, Todos todos) {
-
+        this.port = port;
+        this.todos = todos;
     }
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-
+        while (true) {
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("New connection accepted");
+            final String name = in.readLine();
+        }
     }
 }
